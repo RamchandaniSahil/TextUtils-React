090f5d4a07d1a32769fb18a6c8c2bb3a707aa561
Sahil <shahilramchandnai@gmail.com>
2022-11-01 21:19:39 +0530

    First Version

---
+++ modual1.mjs
@@ -0,0 +1,5 @@
+import dza, {a, c, d} from './modual2.mjs'
+console.log(dza);
+console.log(c);
+console.log(d);
+console.log(a);
--- src/App.js
+++ src/App.js
@@ -1,25 +1,109 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Alert from './components/Alert';
+import About from './components/About';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
+  const [mode, setMode] = useState('light'); // whether dark mod is enable or not
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 1500);
+  }
+
+  const toggleMode = () => {
+    if(mode === 'light') {
+      setMode('dark')
+      document.body.style.backgroundColor = '#042743'
+      showAlert("Dark mode has been enabled", "success")
+      document.title = "TextUtils - Dark Mode"
+    }
+    else {
+      setMode('light')
+      document.body.style.backgroundColor = 'white'
+      showAlert("Light mode has been enabled", "success")
+      document.title = "TextUtils - Light Mode"
+    }
+  }
+
+  const otherMode = () => {
+    if(mode === 'light') {
+      setMode('dark')
+      document.body.style.backgroundColor = '#000000'
+      showAlert("Dark mode has been enabled", "success")
+      document.title = "TextUtils - Dark Mode"
+    }
+    else {
+      setMode('light')
+      document.body.style.backgroundColor = 'white'
+      showAlert("Dark mode has been enabled", "success")
+      document.title = "TextUtils - Light Mode"
+    }
+  }
+
+  const oneMoreOtherMode = () => {
+    if(mode === 'light') {
+      setMode('dark')
+      document.body.style.backgroundColor = '#1a4b50'
+      showAlert("Dark mode has been enabled", "success")
+      document.title = "TextUtils - Dark Mode"
+    }
+    else {
+      setMode('light')
+      document.body.style.backgroundColor = 'white'
+      showAlert("Dark mode has been enabled", "success")
+      document.title = "TextUtils - Light Mode"
+    }
+  }
+
+  const ExtraOtherMode = () => {
+    if(mode === 'light') {
+      setMode('dark')
+      document.body.style.backgroundColor = '#9d339f'
+      showAlert("Dark mode has been enabled", "success")
+      document.title = "TextUtils - Dark Mode"
+      // setInterval(() => {
+      //   document.title = "TextUtils is Amazing Mode"
+      // }, 2000);
+      // setInterval(() => {
+      //   document.title = "Install TextUtils Now"
+      // }, 1500);
+    }
+    else {
+      setMode('light')
+      document.body.style.backgroundColor = 'white'
+      showAlert("Dark mode has been enabled", "success")
+      document.title = "TextUtils - Light Mode"
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar title="TextUtils" mode={mode} toggleMode = {toggleMode} otherMode = {otherMode} oneMoreOtherMode = {oneMoreOtherMode} ExtraOtherMode = {ExtraOtherMode} />
+      <Alert alert={alert} />
+      <div className="container my-3">
+        <Routes>
+          <Route exact path="/about" element={<About />}/>
+          <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}/>}/>
+        </Routes>
+      </div>
+    </Router>
   );
 }
+
 
 export default App;
