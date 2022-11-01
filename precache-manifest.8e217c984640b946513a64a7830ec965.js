6b86a33536a355a83c58c91c8a9a9fd841d1efe5
RamchandaniSahil <109071825+RamchandaniSahil@users.noreply.github.com>
2022-11-01 22:40:32 +0530

    Updates

---
+++ precache-manifest.8e217c984640b946513a64a7830ec965.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6e196b8a0eabbd9a94c",
+    "url": "/TextUtils-React/static/css/main.0b0c6ef1.chunk.css"
+  },
+  {
+    "revision": "b6e196b8a0eabbd9a94c",
+    "url": "/TextUtils-React/static/js/main.b6e196b8.chunk.js"
+  },
+  {
+    "revision": "95ea83b6c7492a61ef7b",
+    "url": "/TextUtils-React/static/js/1.95ea83b6.chunk.js"
+  },
+  {
+    "revision": "31d77044af729c125605",
+    "url": "/TextUtils-React/static/js/2.31d77044.chunk.js"
+  },
+  {
+    "revision": "fb119b557547dc903a9f",
+    "url": "/TextUtils-React/static/js/runtime~main.fb119b55.js"
+  },
+  {
+    "revision": "04068033e7d92c5ad8bf2e8cf09bddc7",
+    "url": "/TextUtils-React/index.html"
+  }
+];
